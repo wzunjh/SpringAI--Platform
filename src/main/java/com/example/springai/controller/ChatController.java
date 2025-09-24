7ae9780e870883ce2e274cd330149a3a07c499ad
wzunjh <wzunjh@qq.com>
2025-09-24 23:51:58 +1000

    会话记忆

--- src/main/java/com/example/springai/controller/ChatController.java
+++ src/main/java/com/example/springai/controller/ChatController.java
@@ -2,6 +2,7 @@ package com.example.springai.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -14,11 +15,13 @@ public class ChatController {
     private final ChatClient chatClient;
 
     @RequestMapping(value = "/chat",produces = "text/html;charset=utf-8")
-    public Flux<String> chat(String prompt){
+    public Flux<String> chat(String prompt,String chatId){
 
-        return chatClient.prompt().user(prompt).stream().content();
-
-
+        return chatClient.prompt()
+                .user(prompt)
+                .advisors(a->a.param(ChatMemory.CONVERSATION_ID,chatId))
+                .stream()
+                .content();
     }
 
 }
