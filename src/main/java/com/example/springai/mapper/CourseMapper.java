8f2bf01692a775df94e46c385b93b29fab03073f
wzunjh <wzunjh@qq.com>
2025-09-26 18:09:13 +1000

    根据条件查询课程

---
+++ src/main/java/com/example/springai/mapper/CourseMapper.java
@@ -0,0 +1,16 @@
+package com.example.springai.mapper;
+
+import com.example.springai.entity.po.Course;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 学科表 Mapper 接口
+ * </p>
+ *
+ * @author uqjoe
+ * @since 2025-09-26
+ */
+public interface CourseMapper extends BaseMapper<Course> {
+
+}
---
+++ src/main/java/com/example/springai/service/impl/SchoolServiceImpl.java
@@ -0,0 +1,20 @@
+package com.example.springai.service.impl;
+
+import com.example.springai.entity.po.School;
+import com.example.springai.mapper.SchoolMapper;
+import com.example.springai.service.ISchoolService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 校区表 服务实现类
+ * </p>
+ *
+ * @author uqjoe
+ * @since 2025-09-26
+ */
+@Service
+public class SchoolServiceImpl extends ServiceImpl<SchoolMapper, School> implements ISchoolService {
+
+}
